b9d4b0e1298989dd1802bd40fd9ea5c099be93ba
Atharva Awari <atharvaawari01@gmail.com>
2024-01-26 22:43:21 +0530

    started popularProduct section

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,7 +1,17 @@
+import {products} from '../constants'
 
 const PopularProducts = () => {
   return (
-    <div>PopularProducts</div>
+    <section id='products' className='max-container max-sm:mt-12' >
+      <div className='flex flex-col justify-start gap-5' >
+        <h2 className='text-4xl font-palanquin font-bold'>Our
+         <span className='text-coral-red' > Poluplar </span> 
+         Porducts</h2>
+        <p>Experience top-notch quality and style with our sought-after selections. Discover a world of comfort, design, and value.</p>
+        {/* time stamp 1:51 */}
+      </div>
+
+    </section>
   )
 }
 
