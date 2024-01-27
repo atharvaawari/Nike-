8a1b9172e3ea50e7b7872ea88d35e381bd1cefd5
Atharva Awari <atharvaawari01@gmail.com>
2024-01-27 22:42:35 +0530

    completed superQuality section and modify button add condition for img

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -3,7 +3,7 @@ import {products} from '../constants'
 
 const PopularProducts = () => {
   return (
-    <section id='products' className='max-container max-sm:mt-12' >
+    <section id='products' className='max-container max-sm:mt-12 flex flex-col' >
       <div className='flex flex-col justify-start gap-5' >
         <h2 className='text-4xl font-palanquin font-bold'>Our
          <span className='text-coral-red' > Poluplar </span> 
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,7 +1,37 @@
+import {shoe8} from '../assets/images'
+import Button from "../components/Button"
 
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container">
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We Provide You <span className="text-coral-red">Super</span> <br /> <span className="text-coral-red">Quality</span> Shoes
+        </h2>
+
+        <p className="mt-4 lg:max-w-lg info-text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et aliquam inventore accusantium officia, autem repellat ipsa. Tempora doloribus autem iste quas, sunt corporis nostrum libero perferendis?
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to details and excellence ensure your satisfaction
+        </p>
+        <div className="mt-11">
+        <Button label="view details" />
+        </div>
+      </div>
+
+      <div className='flex flex-1 justify-center items-center' >
+        <img 
+        src={shoe8} 
+        alt="shoe8"
+        width={570}
+        height={522}
+        className='object-contain'
+        />
+      </div>
+    </section>
   )
 }
 
