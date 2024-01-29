349ca53ac973d28ef855b4fecf5d1a393898f67d
Atharva Awari <atharvaawari01@gmail.com>
2024-01-29 14:47:37 +0530

    completed Subscribe section and add condition of width in button compo

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,10 @@
 
-const Button = ({ label, iconUrl, backgroundColor, textColor, borderColor }) => {
+const Button = ({ label, iconUrl, backgroundColor, textColor, borderColor, fullWidth }) => {
   return (
     <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none 
     ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : "bg-coral-red text-white border-coral-red"
-      } rounded-full w-full}`}>
+      } rounded-full ${fullWidth && 'w-full'}"}`}
+      >
       {label}
 
       {iconUrl && <img
